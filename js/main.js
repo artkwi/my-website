e4fb42dfc6bb269961c91da4841d95908de9470a
unknown <artkwi@st.amu.edu.pl>
2019-03-07 16:50:00 +0100

    Horizontal progrssbar added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+// navigation menu
 var hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', function() {
     document.querySelector('.page-header').classList.toggle('navigation-opened');
 
 }, false);
 
-
+// smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -15,3 +16,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         document.querySelector('.page-header').classList.toggle('navigation-opened');
     });
 });
+
+// progress bar
+
+var progressbar = document.querySelector('.progress-bar');
+
+const statusBar = () => {
+    var currentScroll = Math.round((window.scrollY) / (document.documentElement.offsetHeight -  window.innerHeight)* 100) ;
+    console.log(currentScroll);
+    progressbar.style.width = currentScroll + '%';
+}
+
+window.addEventListener('scroll', statusBar);
